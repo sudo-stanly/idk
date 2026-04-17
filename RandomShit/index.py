f786f4ae668b9df06b8649e71e88f6bdf04c5299
sudo-stanly <alys.git24@gmail.com>
2026-04-17 16:49:29 +0800

    list comprehension

---
+++ RandomShit/index.py
@@ -0,0 +1,14 @@
+# print("wtf")
+
+
+# def HAKDOG():
+#     return f"HAKDOG"
+    
+# print(HAKDOG())
+
+
+user = input("Enter a number: ")
+# print(user.split())
+
+newList = [char for char in user]
+print(newList)
